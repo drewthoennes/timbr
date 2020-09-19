7f9f713b4e65ef0032496dc4822fbbee27bbb507
Anvi Mehta <mehta109@purdue.edu>
2020-09-19 13:01:29 -0400

    inital google signin

--- src/frontend/containers/LoginPage/index.jsx
+++ src/frontend/containers/LoginPage/index.jsx
@@ -3,21 +3,47 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import map from '../../store/map';
 import './styles.scss';
+import {
+  useSession,
+  signin,
+  signout
+} from 'next-auth/client'
+
 
 class LoginPage extends React.Component {
   constructor() {
     super();
+    
 
     this.state = {};
   }
 
+
+
   render() {
+
     return (
       <div id="login-page">
-        <p>timbr login page</p>
+        <p>hi bitch</p>
       </div>
     );
   }
 };
 
-export default connect(map)(withRouter(LoginPage));
+// export default connect(map)(withRouter(LoginPage));
+
+export default function Page() 
+{
+  const [ session, loading ] = useSession()
+
+  return <>
+    {!session && <>
+      Not signed in <br/>
+      <button onClick={signin}>Sign in</button>
+    </>}
+    {session && <>
+      Signed in as {session.user.email} <br/>
+      <button onClick={signout}>Sign out</button>
+    </>}
+  </>
+};
