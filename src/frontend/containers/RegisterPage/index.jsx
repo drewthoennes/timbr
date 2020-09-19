b08745aebe105be7cb14fccdc3e511eb0774c878
Tanya Khandelwal <tkhandel@purdue.edu>
2020-09-18 21:40:40 -0400

    Register front end

--- src/frontend/containers/RegisterPage/index.jsx
+++ src/frontend/containers/RegisterPage/index.jsx
@@ -14,9 +14,57 @@ class RegisterPage extends React.Component {
   render() {
     return (
       <div id="register-page">
-        <p>timbr register page</p>
+        <title>Register</title>
+        <RegisterForm />
       </div>
     );
+  }
+}
+
+class RegisterForm extends React.Component {
+  constructor () {
+      super();
+
+      this.state = {
+          email: '',
+          password: '',
+          username: ''
+      }
+  }
+
+  handleChange = e => {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  handleRegister() {
+    // TODO
+    console.log("Handle register")
+  }
+
+  render() {
+      return (
+          <form>
+            <input type="text"
+            value={ this.state.email }
+            name="email"
+            placeholder="Email"
+            onChange={ this.handleChange }/>
+
+            <input type="text"
+            value={ this.state.username }
+            name="username"
+            placeholder="Username"
+            onChange={ this.handleChange }/>
+
+            <input type="password"
+            value={ this.state.password }
+            name="password"
+            placeholder="Password"
+            onChange={ this.handleChange }/>
+
+            <button onClick = { this.handleRegister }>Register</button>
+          </form>
+      )
   }
 }
 
