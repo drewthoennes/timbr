c32379f45872cb0c58a2f61045564f9d08acf9e0
Tanya Khandelwal <tkhandel@purdue.edu>
2020-09-21 15:37:15 -0400

    Undoing changes

--- src/frontend/containers/RegisterPage/index.jsx
+++ src/frontend/containers/RegisterPage/index.jsx
@@ -14,58 +14,8 @@ class RegisterPage extends React.Component {
   render() {
     return (
       <div id="register-page">
-        <title>Register</title>
-        <RegisterForm />
+        <title>timbr register page</title>
       </div>
     );
   }
 }
-
-class RegisterForm extends React.Component {
-  constructor () {
-      super();
-
-      this.state = {
-          email: '',
-          password: '',
-          username: ''
-      }
-  }
-
-  handleChange = e => {
-    this.setState({[e.target.name]: e.target.value});
-  }
-
-  handleRegister() {
-    // TODO
-    console.log("Handle register")
-  }
-
-  render() {
-      return (
-          <form>
-            <input type="text"
-            value={ this.state.email }
-            name="email"
-            placeholder="Email"
-            onChange={ this.handleChange }/>
-
-            <input type="text"
-            value={ this.state.username }
-            name="username"
-            placeholder="Username"
-            onChange={ this.handleChange }/>
-
-            <input type="password"
-            value={ this.state.password }
-            name="password"
-            placeholder="Password"
-            onChange={ this.handleChange }/>
-
-            <button onClick = { this.handleRegister }>Register</button>
-          </form>
-      )
-  }
-}
-
-export default connect(map)(withRouter(RegisterPage));
