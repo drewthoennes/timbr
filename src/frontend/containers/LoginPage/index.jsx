6668818c605b130517d5da1a0aa88202bbbd9b2d
Anvi Mehta <mehta109@purdue.edu>
2020-09-22 22:23:35 -0400

    google auth complete with firebase

--- src/frontend/containers/LoginPage/index.jsx
+++ src/frontend/containers/LoginPage/index.jsx
@@ -9,6 +9,7 @@ firebase.initializeApp({
   apiKey: "AIzaSyAKPviI0W3v3WuBBxMeZblLcr_t7Y63VAs",
   authDomain: "timbr-cs407.firebaseapp.com",
 });
+<<<<<<< HEAD
 
 
 firebase.initializeApp({
@@ -21,14 +22,19 @@ firebase.initializeApp({
   apiKey: "AIzaSyAKPviI0W3v3WuBBxMeZblLcr_t7Y63VAs",
   authDomain: "timbr-cs407.firebaseapp.com",
 });
+=======
+>>>>>>> google auth complete with firebase
 
 
 class LoginPage extends React.Component {
   constructor() {
     super();
 
+<<<<<<< HEAD
 
 
+=======
+>>>>>>> google auth complete with firebase
 
     this.state = {};
   }
@@ -37,6 +43,9 @@ class LoginPage extends React.Component {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> google auth complete with firebase
 =======
 >>>>>>> google auth complete with firebase
 =======
@@ -50,8 +59,11 @@ class LoginPage extends React.Component {
       (user) => this.setState({ auth: !!user })
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 =======
+=======
+>>>>>>> google auth complete with firebase
 =======
 >>>>>>> google auth complete with firebase
     );
@@ -77,6 +89,7 @@ class LoginPage extends React.Component {
     });
   }
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 >>>>>>> google auth complete with firebase
 =======
@@ -105,10 +118,30 @@ class LoginPage extends React.Component {
 <<<<<<< HEAD
 <<<<<<< HEAD
 =======
+=======
+>>>>>>> google auth complete with firebase
 
 
   render() {
+    if (!this.state.auth) {
+      return (
+        <div>
+          <h1>Click below to sign-in</h1>
+          <button onClick={this.signIn.bind(this)}>SIGN IN</button>
+        </div>
+      );
 
+    }
+      return (
+        <div>
+          <h1>Click below to sign-out</h1>
+          <button onClick={this.signOut.bind(this)}>SIGN OUT</button>
+
+        </div>
+      );
+    
+
+<<<<<<< HEAD
 >>>>>>> inital google signin
     return (
       <div id="login-page">
@@ -203,6 +236,16 @@ export default connect(map)(withRouter(LoginPage));
 >>>>>>> google auth complete with firebase
 =======
 >>>>>>> inital google signin
+=======
+  }
+
+
+};
+
+
+
+export default connect(map)(withRouter(LoginPage));
+>>>>>>> google auth complete with firebase
 =======
   }
 
