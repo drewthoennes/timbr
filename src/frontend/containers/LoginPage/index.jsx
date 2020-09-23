376b0fa9c78b00f4fad911f9ae88ef4ccde9b22a
Anvi Mehta <mehta109@purdue.edu>
2020-09-23 13:08:16 -0400

    added items to package-lock.json

--- src/frontend/containers/LoginPage/index.jsx
+++ src/frontend/containers/LoginPage/index.jsx
@@ -5,74 +5,18 @@ import firebase from 'firebase';
 import map from '../../store/map';
 import './styles.scss';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-firebase.initializeApp({
-  apiKey: "AIzaSyAKPviI0W3v3WuBBxMeZblLcr_t7Y63VAs",
-  authDomain: "timbr-cs407.firebaseapp.com",
-});
-<<<<<<< HEAD
-
-=======
->>>>>>> changes
-
 firebase.initializeApp({
   apiKey: "AIzaSyAKPviI0W3v3WuBBxMeZblLcr_t7Y63VAs",
   authDomain: "timbr-cs407.firebaseapp.com",
 });
 
-
-<<<<<<< HEAD
-=======
->>>>>>> removing  weird errors
-firebase.initializeApp({
-  apiKey: "AIzaSyAKPviI0W3v3WuBBxMeZblLcr_t7Y63VAs",
-  authDomain: "timbr-cs407.firebaseapp.com",
-});
-=======
->>>>>>> google auth complete with firebase
-
-
-=======
->>>>>>> changes
 class LoginPage extends React.Component {
   constructor() {
     super();
-<<<<<<< HEAD
-    
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
-=======
->>>>>>> google auth complete with firebase
-=======
->>>>>>> changes
 
     this.state = {};
   }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> google auth complete with firebase
-=======
->>>>>>> google auth complete with firebase
-=======
->>>>>>> google auth complete with firebase
-=======
->>>>>>> changes
-=======
-    this.state = {};
-  }
-
-
->>>>>>> removing  weird errors
   state = {
     auth: false,
   }
@@ -80,18 +24,10 @@ class LoginPage extends React.Component {
   componentDidMount() {
     this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
       (user) => this.setState({ auth: !!user })
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
-=======
->>>>>>> google auth complete with firebase
-=======
->>>>>>> google auth complete with firebase
     );
   }
+
+
 
   signIn = (event) => {
     console.log(event);
@@ -103,21 +39,16 @@ class LoginPage extends React.Component {
       })
   };
 
-  
+
 
   signOut = () => {
-    firebase.auth().signOut().then(function() {
+    firebase.auth().signOut().then(function () {
       // Sign-out successful.
-    }).catch(function(error) {
+    }).catch(function (error) {
       // An error happened.
     });
   }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> google auth complete with firebase
-=======
->>>>>>> google auth complete with firebase
 
 
   render() {
@@ -130,167 +61,20 @@ class LoginPage extends React.Component {
       );
 
     }
-      return (
-        <div>
-          <h1>Click below to sign-out</h1>
-          <button onClick={this.signOut.bind(this)}>SIGN OUT</button>
-
-        </div>
-      );
-    
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> google auth complete with firebase
-
-
-  render() {
-    if (!this.state.auth) {
-      return (
-        <div>
-          <h1>Click below to sign-in</h1>
-          <button onClick={this.signIn.bind(this)}>SIGN IN</button>
-        </div>
-      );
-
-    }
-      return (
-        <div>
-          <h1>Click below to sign-out</h1>
-          <button onClick={this.signOut.bind(this)}>SIGN OUT</button>
-
-        </div>
-      );
-    
-
-<<<<<<< HEAD
->>>>>>> inital google signin
     return (
-      <div id="login-page">
-        <p>hi bitch</p>
+      <div>
+        <h1>Click below to sign-out</h1>
+        <button onClick={this.signOut.bind(this)}>SIGN OUT</button>
+
       </div>
->>>>>>> inital google signin
-=======
->>>>>>> changes
     );
-  }
 
-
-
-  signIn = (event) => {
-    console.log(event);
-    event.preventDefault();
-    const prv = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(prv)
-      .then(function (result) {
-        var user = result.user;
-      })
-  };
-
-  
-
-  signOut = () => {
-    firebase.auth().signOut().then(function() {
-      // Sign-out successful.
-    }).catch(function(error) {
-      // An error happened.
-    });
-  }
-
-
-
-  render() {
-    if (!this.state.auth) {
-      return (
-        <div>
-          <h1>Click below to sign-in</h1>
-          <button onClick={this.signIn.bind(this)}>SIGN IN</button>
-        </div>
-      );
-
-    }
-      return (
-        <div>
-          <h1>Click below to sign-out</h1>
-          <button onClick={this.signOut.bind(this)}>SIGN OUT</button>
-
-        </div>
-      );
-    
 
   }
 
 
 };
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
 
 export default connect(map)(withRouter(LoginPage));
-=======
-=======
->>>>>>> inital google signin
-// export default connect(map)(withRouter(LoginPage));
 
-export default function Page() 
-{
-  const [ session, loading ] = useSession()
-
-  return <>
-    {!session && <>
-      Not signed in <br/>
-      <button onClick={signin}>Sign in</button>
-    </>}
-    {session && <>
-      Signed in as {session.user.email} <br/>
-      <button onClick={signout}>Sign out</button>
-    </>}
-  </>
-};
-<<<<<<< HEAD
->>>>>>> inital google signin
-=======
-  }
-
-
-};
-
-
-
-export default connect(map)(withRouter(LoginPage));
->>>>>>> google auth complete with firebase
-=======
->>>>>>> inital google signin
-=======
-  }
-
-
-};
-
-
-
-export default connect(map)(withRouter(LoginPage));
->>>>>>> google auth complete with firebase
-=======
-  }
-
-
-};
-
-
-
-export default connect(map)(withRouter(LoginPage));
->>>>>>> google auth complete with firebase
-=======
-
-<<<<<<< HEAD
-
-export default connect(map)(withRouter(LoginPage));
->>>>>>> changes
-=======
-export default connect(map)(withRouter(LoginPage));
->>>>>>> removing  weird errors
