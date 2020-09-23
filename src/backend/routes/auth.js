3f0368bf46d2d684c44972779b1d2c37ee4552de
Tanya Khandelwal <tkhandel@purdue.edu>
2020-09-23 16:14:35 -0400

    Adding endpoint for login

--- src/backend/routes/auth.js
+++ src/backend/routes/auth.js
@@ -5,4 +5,9 @@ module.exports = (router) => {
     firebase.auth().createUserWithEmailAndPassword(req.body.email, req.body.password);
     res.send('User created!');
   });
+
+  router.post('/api/login', (req, res) => {
+    firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password);
+    res.send('User logged in!');
+  });  
 };
