1b8bb4b642cd59ea6c10e34300d3199500acf3b8
Anvi Mehta <mehta109@purdue.edu>
2020-09-22 22:23:35 -0400

    google auth complete with firebase

--- src/frontend/containers/LoginPage/index.jsx
+++ src/frontend/containers/LoginPage/index.jsx
@@ -1,49 +1,80 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import firebase from 'firebase';
 import map from '../../store/map';
 import './styles.scss';
-import {
-  useSession,
-  signin,
-  signout
-} from 'next-auth/client'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAKPviI0W3v3WuBBxMeZblLcr_t7Y63VAs",
+  authDomain: "timbr-cs407.firebaseapp.com",
+});
 
 
 class LoginPage extends React.Component {
   constructor() {
     super();
-    
+
 
     this.state = {};
+  }
+
+  state = {
+    auth: false,
+  }
+
+  componentDidMount() {
+    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+      (user) => this.setState({ auth: !!user })
+    );
+  }
+
+  signIn = (event) => {
+    console.log(event);
+    event.preventDefault();
+    const prv = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(prv)
+      .then(function (result) {
+        var user = result.user;
+      })
+  };
+
+  
+
+  signOut = () => {
+    firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+    }).catch(function(error) {
+      // An error happened.
+    });
   }
 
 
 
   render() {
+    if (!this.state.auth) {
+      return (
+        <div>
+          <h1>Click below to sign-in</h1>
+          <button onClick={this.signIn.bind(this)}>SIGN IN</button>
+        </div>
+      );
 
-    return (
-      <div id="login-page">
-        <p>hi bitch</p>
-      </div>
-    );
+    }
+      return (
+        <div>
+          <h1>Click below to sign-out</h1>
+          <button onClick={this.signOut.bind(this)}>SIGN OUT</button>
+
+        </div>
+      );
+    
+
   }
+
+
 };
 
-// export default connect(map)(withRouter(LoginPage));
 
-export default function Page() 
-{
-  const [ session, loading ] = useSession()
 
-  return <>
-    {!session && <>
-      Not signed in <br/>
-      <button onClick={signin}>Sign in</button>
-    </>}
-    {session && <>
-      Signed in as {session.user.email} <br/>
-      <button onClick={signout}>Sign out</button>
-    </>}
-  </>
-};
+export default connect(map)(withRouter(LoginPage));
