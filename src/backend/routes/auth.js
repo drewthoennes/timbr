2be086856e72d308c74e0c4e22c39238d39e7b0f
Tanya Khandelwal <tkhandel@purdue.edu>
2020-09-22 22:54:41 -0400

    Basic sign up works

---
+++ src/backend/routes/auth.js
@@ -0,0 +1,9 @@
+const firebase = require('firebase')
+
+module.exports = (router) => {
+    router.post('/api/register', (req, res) => {
+        firebase.auth().createUserWithEmailAndPassword(req.body.email, req.body.password)
+        res.send("User created for " + req.body.email+"!")
+    });
+  };
+  
--- src/frontend/containers/RegisterPage/index.jsx
+++ src/frontend/containers/RegisterPage/index.jsx
@@ -3,18 +3,47 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import map from '../../store/map';
 import './styles.scss';
+import axios from 'axios'
+import history from '../../router/history'
+import passwordHash from 'password-hash';
 
 class RegisterPage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.handleRegister = this.handleRegister.bind(this)
+  }
+
+  handleRegister(e) {
+    e.preventDefault();
+
+    const credentials = {
+      email: document.getElementById("email").value,
+      password: passwordHash.generate(document.getElementById("password").value),
+    };
+
+    axios
+      .post('/api/register', credentials)
+      .then(res => console.log(res.data))
+      .catch(err => console.error(err))
+
+      history.push('/login')
+
   }
 
   render() {
     return (
       <div id="register-page">
-        <p>timbr register page</p>
+        <form id="register-form">
+          <input id="email"
+            type="text"
+            placeholder="Email" />
+
+          <input id="password"
+            type="password"
+            placeholder="Password" />
+          <button onClick={this.handleRegister}>Register</button>
+        </form>
       </div>
     );
   }
