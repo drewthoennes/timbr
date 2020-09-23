b7dcf35e504dc1c3be29d211a7678040e80f659e
Anvi Mehta <mehta109@purdue.edu>
2020-09-22 22:23:35 -0400

    google auth complete with firebase

--- src/frontend/containers/LoginPage/index.jsx
+++ src/frontend/containers/LoginPage/index.jsx
@@ -4,11 +4,11 @@ import { withRouter } from 'react-router-dom';
 import firebase from 'firebase';
 import map from '../../store/map';
 import './styles.scss';
-import {
-  useSession,
-  signin,
-  signout
-} from 'next-auth/client'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAKPviI0W3v3WuBBxMeZblLcr_t7Y63VAs",
+  authDomain: "timbr-cs407.firebaseapp.com",
+});
 
 
 firebase.initializeApp({
@@ -20,13 +20,16 @@ firebase.initializeApp({
 class LoginPage extends React.Component {
   constructor() {
     super();
-    
+
 
 
     this.state = {};
   }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> google auth complete with firebase
   state = {
     auth: false,
   }
@@ -34,11 +37,55 @@ class LoginPage extends React.Component {
   componentDidMount() {
     this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
       (user) => this.setState({ auth: !!user })
+<<<<<<< HEAD
 =======
+=======
+    );
+  }
+
+  signIn = (event) => {
+    console.log(event);
+    event.preventDefault();
+    const prv = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(prv)
+      .then(function (result) {
+        var user = result.user;
+      })
+  };
+
+  
+
+  signOut = () => {
+    firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+    }).catch(function(error) {
+      // An error happened.
+    });
+  }
+
+>>>>>>> google auth complete with firebase
 
 
   render() {
+    if (!this.state.auth) {
+      return (
+        <div>
+          <h1>Click below to sign-in</h1>
+          <button onClick={this.signIn.bind(this)}>SIGN IN</button>
+        </div>
+      );
 
+    }
+      return (
+        <div>
+          <h1>Click below to sign-out</h1>
+          <button onClick={this.signOut.bind(this)}>SIGN OUT</button>
+
+        </div>
+      );
+    
+
+<<<<<<< HEAD
     return (
       <div id="login-page">
         <p>hi bitch</p>
@@ -116,3 +163,13 @@ export default function Page()
   </>
 };
 >>>>>>> inital google signin
+=======
+  }
+
+
+};
+
+
+
+export default connect(map)(withRouter(LoginPage));
+>>>>>>> google auth complete with firebase
