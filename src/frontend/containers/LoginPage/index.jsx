49d8a9990db12f6d6db06520abf702096c23c301
drewthoennes <dthoenne@purdue.edu>
2020-09-24 16:05:55 -0400

    Fixes linting

--- src/frontend/containers/LoginPage/index.jsx
+++ src/frontend/containers/LoginPage/index.jsx
@@ -6,87 +6,85 @@ import map from '../../store/map';
 import './styles.scss';
 
 firebase.initializeApp({
-  apiKey: "AIzaSyAKPviI0W3v3WuBBxMeZblLcr_t7Y63VAs",
-  authDomain: "timbr-cs407.firebaseapp.com",
+  apiKey: 'AIzaSyAKPviI0W3v3WuBBxMeZblLcr_t7Y63VAs',
+  authDomain: 'timbr-cs407.firebaseapp.com',
 });
+
+function signInGoogle(event) {
+  // console.log(event);
+  event.preventDefault();
+  const prv = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(prv);
+  /*
+    .then((result) => {
+      const { user } = result;
+    });
+  */
+}
+
+function signInFacebook() {
+  const provider = new firebase.auth.FacebookAuthProvider();
+  // firebase.auth().signInWithPopup(provider).then((result) => {
+  firebase.auth().signInWithPopup(provider).then(() => {
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    // const token = result.credential.accessToken;
+    // The signed-in user info.
+    // const { user } = result;
+    // ...
+  // }).catch((error) => {
+  }).catch(() => {
+    // Handle Errors here.
+    // const errorCode = error.code;
+    // const errorMessage = error.message;
+    // The email of the user's account used.
+    // const { email } = error;
+    // The firebase.auth.AuthCredential type that was used.
+    // const { credential } = error;
+    // ...
+  });
+}
+
+function signOut() {
+  firebase.auth().signOut().then(() => {
+    // Sign-out successful.
+  }).catch(() => {
+    // An error happened.
+  });
+}
 
 class LoginPage extends React.Component {
   constructor() {
     super();
-    this.state = {};
-  }
-
-  state = {
-    auth: false,
+    this.state = {
+      auth: false,
+    };
   }
 
   componentDidMount() {
     this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
-      (user) => this.setState({ auth: !!user })
+      (user) => this.setState({ auth: !!user }),
     );
   }
 
-  signInGoogle = (event) => {
-    console.log(event);
-    event.preventDefault();
-    const prv = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(prv)
-      .then(function (result) {
-        var user = result.user;
-      })
-  };
-
-  signInFacebook = () => {
-
-    var provider = new firebase.auth.FacebookAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-      var token = result.credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      // ...
-    }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
-    });
-  }
-
-
-  signOut = () => {
-    firebase.auth().signOut().then(function () {
-      // Sign-out successful.
-    }).catch(function (error) {
-      // An error happened.
-    });
-  }
-
-
-
   render() {
-    if (!this.state.auth) {
+    const { auth } = this.state;
+
+    if (!auth) {
       return (
         <div>
           <h1>Click below to sign-in</h1>
-          <button onClick={this.signInGoogle.bind(this)}>SIGN IN WITH GOOGLE</button>
-          <button onClick={this.signInFacebook.bind(this)}>SIGN IN WITH FACEBOOK</button>
+          <button type="button" onClick={signInGoogle}>SIGN IN WITH GOOGLE</button>
+          <button type="button" onClick={signInFacebook}>SIGN IN WITH FACEBOOK</button>
         </div>
       );
     }
     return (
       <div>
         <h1>Click below to sign-out</h1>
-        <button onClick={this.signOut.bind(this)}>SIGN OUT</button>
+        <button type="button" onClick={signOut}>SIGN OUT</button>
 
       </div>
     );
-
-
   }
 }
 export default connect(map)(withRouter(LoginPage));
