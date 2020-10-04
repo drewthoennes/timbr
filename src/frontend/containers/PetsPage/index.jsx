dc5027473fc5a2ed00a1398668d9a934526328ba
Tanya Khandelwal <tkhandel@purdue.edu>
2020-10-04 10:47:51 -0400

    Adding logout

--- src/frontend/containers/PetsPage/index.jsx
+++ src/frontend/containers/PetsPage/index.jsx
@@ -1,15 +1,32 @@
+/* eslint-disable no-console */
+
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import map from '../../store/map';
 import './styles.scss';
+import accountActions from '../../store/actions/account';
 
 class PetsPage extends React.Component {
   constructor() {
     super();
 
     this.state = {};
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  handleLogout(e) {
+    e.preventDefault();
+
+    const { history } = this.props;
+    accountActions.logout()
+      .then(() => {
+        history.push('/login');
+      })
+      .catch((error) => {
+        console.log(`Error: ${error.message}`);
+      });
   }
 
   render() {
@@ -23,12 +40,20 @@ class PetsPage extends React.Component {
       <div id="pets-page">
         <h1>timbr Pets Page</h1>
         { petsJsx }
+        <button
+          id="logout"
+          type="button"
+          onClick={this.handleLogout}
+        >
+          Logout
+        </button>
       </div>
     );
   }
 }
 
 PetsPage.propTypes = {
+  history: PropTypes.object.isRequired,
   store: PropTypes.shape({
     pets: PropTypes.object.isRequired,
   }).isRequired,
