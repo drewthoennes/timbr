853610ae0fbefdf12dc63c67e3f07364e4a96580
drewthoennes <dthoenne@purdue.edu>
2020-10-05 17:25:59 -0400

    Moves Firebase service account to environment variables for CI/CD

--- test/backend/utils/server.js
+++ test/backend/utils/server.js
@@ -1,14 +1,9 @@
 const express = require('express');
-const nodemailer=require('nodemailer');
-const nodemailMailgun=require('nodemailer-mailgun-transport');
 const bodyParser = require('body-parser');
 const apiRoutes = require('../../../src/backend/routes');
 
 let app;
 let session;
-
-
-
 
 const createApp = () => {
   app = express();
@@ -29,18 +24,15 @@ const createApp = () => {
   return app;
 };
 
-
-
 const createSession = () => {
   if (app) {
-    
     session = app.listen(42014); // Needs to listen on an unused port
   }
 };
 
 const killSession = () => {
   if (session) {
-    return session.close();
+    session.close();
   }
 };
 
