d28d713ecd47e116c829979be2eccd6e8de334af
Anvi Mehta <32017801+anvimehta@users.noreply.github.com>
2020-10-07 19:58:29 -0400

    toggle alerts (#24)
    * set up sms/email notifs- temporary change
    
    * moved alerts code to backend
    
    * set up sms and text alerts - all done
    
    * fixed linting error
    
    * fixed console linting errors
    
    * modified to fetch changes under /users once
    
    * resolve lint errors for resolving path
    
    * fixed linting errors
    
    * email and text notifications in firebase and toggle to turn each on and off in accounts settings page
    
    * lint fixes
    
    * review changes
    
    * changing code based on review
    
    * Update alerts.js
    
    * review changes
    
    Co-authored-by: niharika01 <npujar@purdue.edu>

--- src/backend/alerts/alerts.js
+++ src/backend/alerts/alerts.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 const nodemailer = require('nodemailer');
-// const cron = require('node-cron');
+const cron = require('node-cron');
 const Twilio = require('twilio');
 const admin = require('firebase-admin');
 
@@ -24,20 +24,20 @@ function sendNotificationText(userPhoneNumber, textContent) {
   const accountSid = process.env.TWILIO_SID;
   const authToken = process.env.TWILIO_AUTHTOKEN;
   const client = new Twilio(accountSid, authToken);
-  // cron.schedule('*/10 * * * *', function() {
+  // cron.schedule('*/1 * * * *', function() {
   client.messages.create({
     body: textContent,
     to: userPhoneNumber, // Text this number
     from: process.env.PHONE_NUMBER, // From a valid Twilio number
   })
     .then((message) => console.log(message.sid));
-// console.log('running a cron sms task every 10 minutes');
-// });//cron task 2
+  // console.log('running a cron sms task every 10 minutes');
+  // });//cron task 2
 }
 
 // set up Email alerts
 function sendNotificationEmail(emailAddress, textContent) {
-  // cron.schedule('*/10 * * * *', function() {
+  // cron.schedule('*/1 * * * *', function() {
   transporter.sendMail(mailOptions(emailAddress, textContent), (error, info) => {
     if (error) {
       console.log(error);
@@ -45,30 +45,40 @@ function sendNotificationEmail(emailAddress, textContent) {
       console.log(`Email sent: ${info.response}`, `email sent to ${emailAddress}`);
     }
   });
-// console.log('running a cron email task every 10 minutes');
-// });//cron task 2
+  // console.log('running a cron email task every 10 minutes');
+  // });//cron task 2
 }
 
 // Fetch the user's email and send an email to everyone registered on the database.
 const reminders = ['water', 'fertilize', 'rotate'];
-const plantsRef = admin.database().ref('/users/');
-plantsRef.once('value', (snapshot0) => {
-  snapshot0.forEach((snapshot1) => {
-    const userEmail = snapshot1.val().email;
-    snapshot1.forEach((snapshot2) => {
-      snapshot2.forEach((e) => {
-        const plantName = e.val().name;
-        const userPhoneNumber = '+13129521148';
-        reminders.forEach((r) => {
-          const textBody = `Hello from timbr,\nThis is a friendly reminder to ${r} ${plantName} 🌱`;
 
-          if (process.env.SEND_EMAILS === 'true') {
-            sendNotificationEmail(userEmail, textBody); // send email notification
-          }
+// Schedule for texts and emails every 10 minutes
+cron.schedule('*/10 * * * *', () => {
+  const plantsRef = admin.database().ref('/users/');
+  plantsRef.once('value', (snapshot0) => {
+    snapshot0.forEach((snapshot1) => {
+      const userEmail = snapshot1.val().email;
 
-          if (process.env.SEND_TEXTS === 'true') {
-            sendNotificationText(userPhoneNumber, textBody);// send text notification
-          }
+      // Check to see status of texts and emails
+      const { textsOn } = snapshot1.val();
+      const { emailsOn } = snapshot1.val();
+
+      snapshot1.forEach((snapshot2) => {
+        snapshot2.forEach((e) => {
+          const plantName = e.val().name;
+          const userPhoneNumber = '+17657759145';
+
+          reminders.forEach((r) => {
+            const textBody = `Hello from timbr,\nThis is a friendly reminder to ${r} ${plantName} 🌱`;
+
+            if (emailsOn === true && process.env.SEND_EMAILS === 'true') {
+              sendNotificationEmail(userEmail, textBody); // send email notification
+            }
+
+            if (textsOn === true && process.env.SEND_TEXTS === 'true') {
+              sendNotificationText(userPhoneNumber, textBody);// send text notification
+            }
+          });
         });
       });
     });
