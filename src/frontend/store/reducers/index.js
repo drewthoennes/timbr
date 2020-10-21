4def71dfcd02171041b877f29a593260f14682ea
Drew Thoennes <dthoenne@purdue.edu>
2020-10-20 21:12:29 -0400

    Adds static plant data (#29)
    * Adds static data
    
    * Adds more plants to static file
    
    * Adds more succulents
    
    * Adds constant rotate frequency to file
    
    * Adds plants to store

--- src/frontend/store/reducers/index.js
+++ src/frontend/store/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 
 import account from './account';
 import pets from './pets';
+import plants from './plants';
 import users from './users';
 
 const reducers = combineReducers({
   account,
   pets,
+  plants,
   users,
 });
 
