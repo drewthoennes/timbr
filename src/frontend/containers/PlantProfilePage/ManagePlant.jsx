f516886893b982bb91fc0a90846d4e840fe7a979
Tanya Khandelwal <tkhandel@purdue.edu>
2020-11-12 21:16:10 -0500

    Changes added to the plant profile page

--- src/frontend/containers/PlantProfilePage/ManagePlant.jsx
+++ src/frontend/containers/PlantProfilePage/ManagePlant.jsx
@@ -9,17 +9,26 @@ class ManagePlant extends React.PureComponent {
     super();
 
     this.state = {
-      show: false,
+      showDelete: false,
+      showDead: false,
     };
 
     this.toggleDeleteModal = this.toggleDeleteModal.bind(this);
+    this.toggleDeadModal = this.toggleDeadModal.bind(this);
     this.edit = this.edit.bind(this);
     this.delete = this.delete.bind(this);
+    this.dead = this.dead.bind(this);
   }
 
   toggleDeleteModal() {
     this.setState((prevState) => ({
-      show: !prevState.show,
+      showDelete: !prevState.showDelete,
+    }));
+  }
+
+  toggleDeadModal() {
+    this.setState((prevState) => ({
+      showDead: !prevState.showDead,
     }));
   }
 
@@ -39,17 +48,26 @@ class ManagePlant extends React.PureComponent {
     });
   }
 
+  dead() {
+    const { history, username } = this.props;
+
+    this.toggleDeadModal();
+    // TODO: Change this redirection to graveyard
+    history.push(`/${username}`);
+  }
+
   render() {
     const { pet } = this.props;
-    const { show } = this.state;
+    const { showDelete, showDead } = this.state;
 
     return (
       <div>
         <h2>Manage Plant</h2>
         <Button type="button" onClick={this.edit}>Edit</Button>
         <Button type="button" onClick={this.toggleDeleteModal}>Delete</Button>
+        <Button type="button" onClick={this.toggleDeadModal}>Ded</Button>
 
-        <Modal show={show} onHide={this.toggleDeleteModal}>
+        <Modal show={showDelete} onHide={this.toggleDeleteModal}>
           <Modal.Header closeButton>
             <Modal.Title>Delete {pet?.name}</Modal.Title>
           </Modal.Header>
@@ -57,6 +75,17 @@ class ManagePlant extends React.PureComponent {
           <Modal.Footer>
             <Button variant="secondary" onClick={this.toggleDeleteModal}>No</Button>
             <Button variant="primary" onClick={this.delete}>Yes</Button>
+          </Modal.Footer>
+        </Modal>
+
+        <Modal show={showDead} onHide={this.toggleDeadModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>We're sorry about your loss.</Modal.Title>
+          </Modal.Header>
+          <input type="text" placeholder="Add an epitaph to remember your plant, if you'd like." />
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.toggleDeadModal}>Wait, not dead yet!</Button>
+            <Button variant="primary" onClick={this.dead}>Confirm</Button>
           </Modal.Footer>
         </Modal>
       </div>
