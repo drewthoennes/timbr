de30d2be5985810be99ff28a27191b4bc4b8fd54
niharika01 <npujar@purdue.edu>
2020-12-04 02:40:45 +0530

    diffDays shows days>0

--- src/frontend/containers/PlantProfilePage/index.jsx
+++ src/frontend/containers/PlantProfilePage/index.jsx
@@ -144,7 +144,7 @@ class PlantProfilePage extends React.Component {
     const dt2 = new Date(date);
     let diffDays = Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate())
     - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate())) / (1000 * 60 * 60 * 24));
-    if (diffDays > daysToAdd) {
+    if (diffDays > daysToAdd || diffDays < 0) {
       diffDays = 0;
     }
 
